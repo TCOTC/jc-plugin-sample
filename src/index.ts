85af954482e898c466bc28fecd59b5fe9416d649
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2026-07-30 01:55:03 +0800

    适配思源 v3.7 并升级依赖

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,39 @@ import {
     Plugin,
     showMessage,
 } from "siyuan";
+import type zhCN from "./i18n/zh-CN.json";
 import "./index.scss";
 
 const STORAGE_NAME = "config.json";
 
 export default class JCPlugin extends Plugin {
+    declare i18n: typeof zhCN;
+
     onload() {
         this.loadData(STORAGE_NAME).catch(e => {
-            console.log(`[${this.name}] load data [${STORAGE_NAME}] fail: `, e);
+            const errorMessage = `${this.displayName}: failed to load data [${STORAGE_NAME}]: ${e.msg}`;
+            showMessage(errorMessage);
+            console.error(errorMessage);
         });
-        console.log(this.displayName + ": loaded");
+
+        console.log(this.displayName, "plugin loaded");
     }
 
     onLayoutReady() {
-        console.log(this.displayName + ": layout ready");
+        console.log(this.displayName, "plugin layout ready");
     }
 
     onunload() {
-        console.log(this.displayName + ": unloaded");
+        console.log(this.displayName, "plugin unloaded");
     }
 
     uninstall() {
         this.removeData(STORAGE_NAME).catch(e => {
-            showMessage(`uninstall [${this.name}] remove data [${STORAGE_NAME}] fail: ${e.msg}`);
+            const errorMessage = `${this.displayName}: failed to uninstall remove data [${STORAGE_NAME}]: ${e.msg}`;
+            showMessage(errorMessage);
+            console.error(errorMessage);
         });
-        console.log(this.displayName + ": uninstalled");
+
+        console.log(this.displayName, "plugin uninstalled");
     }
 }
