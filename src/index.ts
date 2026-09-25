15ae18ac66bb67ef33e35d1f73f1c176521ea710
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2026-09-25 22:55:09 +0800

    更新依赖、添加 onDataChanged 生命周期方法、为所有生命周期方法添加注释

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ const STORAGE_NAME = "config.json";
 export default class JCPlugin extends Plugin {
     declare i18n: typeof zhCN;
 
+    /**
+     * 插件启用时调用
+     */
     onload() {
         this.loadData(STORAGE_NAME).catch(e => {
             const errorMessage = `${this.displayName}: failed to load data [${STORAGE_NAME}]: ${e.msg}`;
@@ -20,14 +23,34 @@ export default class JCPlugin extends Plugin {
         console.log(this.displayName, "plugin loaded");
     }
 
+    /**
+     * 思源界面布局就绪，始终在 onload 执行完之后调用
+     */
     onLayoutReady() {
         console.log(this.displayName, "plugin layout ready");
     }
 
+    /**
+     * 插件使用 this.saveData 保存的数据发生变化时调用，包括数据同步之后、其他前端实例改变数据之后
+     */
+    onDataChanged() {
+        this.loadData(STORAGE_NAME).catch(e => {
+            const errorMessage = `${this.displayName}: failed to load data [${STORAGE_NAME}]: ${e.msg}`;
+            showMessage(errorMessage);
+            console.error(errorMessage);
+        });
+    }
+
+    /**
+     * 插件禁用时调用
+     */
     onunload() {
         console.log(this.displayName, "plugin unloaded");
     }
 
+    /**
+     * 插件卸载时调用，会先调用完 onunload
+     */
     uninstall() {
         this.removeData(STORAGE_NAME).catch(e => {
             const errorMessage = `${this.displayName}: failed to uninstall remove data [${STORAGE_NAME}]: ${e.msg}`;
